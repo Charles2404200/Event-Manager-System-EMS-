0ab4e7a1e79b2962f143fc81be4ef530f6b4d71c
Charles2404200 <leanhminh2404@gmail.com>
2025-12-03 01:42:38 +0700

    commit environment

---
+++ src/main/java/org/Main.java
@@ -0,0 +1,14 @@
+package org;
+/**
+ * @author <your group number>
+ */
+
+import org.ems.ui.MainApp;
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        // Launch the JavaFX UI
+        Application.launch(MainApp.class, args);
+    }
+}
